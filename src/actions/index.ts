05b49c6a47d19cd5eb87dfe85380e600ae6e983a
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-08-04 18:22:25 +0200

    create todos reducer

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,15 +2,15 @@ import axios from "axios";
 import { Dispatch } from "redux";
 import { ActionTypes } from "./types";
 
-interface Todo {
+export interface Todo {
   id: number;
   title: string;
   completed: boolean;
 }
 
-interface FetchTodoAction {
-    type: ActionTypes.fetchTodos;
-    payload: Todo[];
+export interface FetchTodosAction {
+  type: ActionTypes.fetchTodos;
+  payload: Todo[];
 }
 
 const url = "http://jsonplaceholder.typecode.com/todos";
@@ -18,8 +18,8 @@ const url = "http://jsonplaceholder.typecode.com/todos";
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
     const response = await axios.get<Todo[]>(url);
-    
-    dispatch<FetchTodoAction>({
+
+    dispatch<FetchTodosAction>({
       type: ActionTypes.fetchTodos,
       payload: response.data,
     });
