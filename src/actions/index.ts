039883154030b12cb63e8272a4324f3ef67fa7cb
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-08-04 18:11:59 +0200

    make dispatch function generic

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -8,12 +8,18 @@ interface Todo {
   completed: boolean;
 }
 
+interface FetchTodoAction {
+    type: ActionTypes.fetchTodos;
+    payload: Todo[];
+}
+
 const url = "http://jsonplaceholder.typecode.com/todos";
 
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
     const response = await axios.get<Todo[]>(url);
-    dispatch({
+    
+    dispatch<FetchTodoAction>({
       type: ActionTypes.fetchTodos,
       payload: response.data,
     });
